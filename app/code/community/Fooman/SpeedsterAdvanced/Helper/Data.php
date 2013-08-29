d3c82bf932283e1ac27862aed1e008f92347fd69
helin16@gmail.com <helin16@gmail.com@e87f2038-72e8-861c-5b4f-c3bc327194f4>
2013-08-29 13:48:20 +0000

    delete

--- app/code/community/Fooman/SpeedsterAdvanced/Helper/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Fooman_SpeedsterAdvanced_Helper_Data extends Mage_Core_Helper_Abstract
-{
-
-}
--- app/code/local/Magentothem/Newproduct/Helper/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Magentothem_Newproduct_Helper_Data extends Mage_Core_Helper_Abstract
-{
-
-}
